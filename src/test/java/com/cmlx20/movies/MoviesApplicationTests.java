f90b124ff2f65ed69c9ebc720b60604045e872d2
Lucian Chiriazi <153869152+Lucian-Chiriazi@users.noreply.github.com>
2024-09-03 22:41:01 +0100

    First Commit

---
+++ src/test/java/com/cmlx20/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmlx20.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
